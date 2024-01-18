7dd3b058977d873192dd03d5f3e2801a2b048db0
Zolzaya99 <zolzayaoyunkhand@gmail.com>
2024-01-18 11:43:14 -0500

    created new folders

---
+++ src/app/shopping-list/shipping-edit/shipping-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shipping-edit',
+  templateUrl: './shipping-edit.component.html',
+  styleUrl: './shipping-edit.component.css'
+})
+export class ShippingEditComponent {
+
+}
